6a451afce10d60320a2fab890d895cbd76e8bfb2
shuvo Haldar <shuvoh38@gmail.com>
2023-11-01 18:48:24 +0600

    added layout of site api doc

--- src/views/GeneralView/Sites/Site.vue
+++ src/views/GeneralView/Sites/Site.vue
@@ -1,17 +1,75 @@
 <template>
-  <div class="container">
+  <div>
     <h2>project site title</h2>
     <div>
         <pre>{{site}}</pre>
         <h2>{{site.info}}</h2>
     </div>
+    <section class="site_container">
+        <aside class="site_nav">
+            side accordian nav 
+            <div>
+                <input type="text" placeholder="search here">
+            </div>
+            <div>
+                <div>
+                    <ul>
+                        <li>
+                            <label for="">Platform</label>
+                            <ul>
+                                <li>
+                                    <label for="">Users API</label>
+                                    <ul>
+                                        <li><label for="">Careate User</label></li>
+                                        <li><label for="">Get User</label></li>
+                                        <li><label for="">Delete User</label></li>
+                                        <li><label for="">Update User</label></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <label for="">Application</label>
+                            <ul>
+                                <li>
+                                    <label for="">Video API</label>
+                                    <ul>
+                                        <li><label for="">Add a Video</label></li>
+                                        <li><label for="">Get Video</label></li>
+                                        <li><label for="">Delete Video</label></li>
+                                        <li><label for="">Update Video</label></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <label for="">API docs by BondBridgeLink Inc.</label>
+                </div>
+            </div>
+        </aside>
+        <aside class="site_details">
+            <div class="row">
+                <div>details 1</div>
+                <div>API Response code snippet response 1</div>
+            </div>
+            <div class="row">
+                <div>details 2</div>
+                <div>
+                    API Response code snippet response 2
+                    <pre style="color:#00FFFF;background-color:#000; width:fit-content;">{{site}}</pre>
+                </div>
+            </div>
+        </aside>
+    </section>
   </div>
 </template>
 
 <script>
 import { useRoute, useRouter } from 'vue-router';
 import {docList} from "@/data/docList.js";
-import { onBeforeMount, reactive, toRefs, watch } from 'vue';
+import { onBeforeMount, reactive, } from 'vue';
 
 export default {
     name:"project-site",
@@ -36,8 +94,19 @@ export default {
 }
 </script>
 
-<style scoped>
-    .container{
-        padding: 0 10%;
+<style lang="scss" scoped>
+    .site_container{
+        display: grid;
+        grid-template-columns: 250px 1fr;
+        gap: 1rem;
+    }
+
+    .site_details{
+        
+        .row{
+            display: grid;
+            grid-template-columns: repeat(2,1fr);
+            gap: 1rem;
+        }
     }
 </style>
